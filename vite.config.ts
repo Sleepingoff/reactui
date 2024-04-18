e7ab77d86f310a122fa39a7c38f6d554fe35e16c
Sleepingoff <justjh30@gmail.com>
2024-04-18 14:35:58 +0900

    상대 경로 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,15 @@ export default defineConfig({
     alias: [
       { find: "@/", replacement: "/src" },
       { find: "@/components", replacement: "/src/components" },
+      { find: "@/components/atoms", replacement: "/src/components/atoms" },
+      {
+        find: "@/components/molecules",
+        replacement: "/src/components/molecules",
+      },
+      {
+        find: "@/components/organisms",
+        replacement: "/src/components/organisms",
+      },
       { find: "@/assets", replacement: "/src/assets" },
       { find: "@/types", replacement: "/src/types" },
       { find: "@/utils", replacement: "/src/utils" },
