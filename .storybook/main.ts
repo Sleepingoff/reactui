edc9b07f1da5699e0afa4f8e8d6444228d6ef787
Sleepingoff <justjh30@gmail.com>
2024-04-22 18:32:28 +0900

    💄 스토리북 addon 추가 설치 및 배포 준비

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,13 @@ import { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   framework: '@storybook/react-vite',
-  addons: ['@storybook/addon-a11y'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource',
+    '@storybook/addon-designs',
+    '@storybook/addon-actions',
+    '@storybook/addon-controls'
+  ],
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   core: {
     disableTelemetry: true // 👈 Disables telemetry
