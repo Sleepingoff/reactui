ec500f13e4887a1973f72f93e4665094d0872f39
Sleepingoff <justjh30@gmail.com>
2024-04-26 01:58:40 +0900

    Tab 컴포넌트 스타일 추가

--- src/components/organisms/Tab/Tab.tsx
+++ src/components/organisms/Tab/Tab.tsx
@@ -2,22 +2,34 @@ import { useMemo, useState } from 'react';
 
 import Panel from '@/components/molecules/Panel/Panel';
 import Title from '@/components/molecules/Title/Title';
+
 import useTab, { TabContext } from '@/hooks/useTab';
+
 import { DataType } from '@/types/Context/TabContext';
 import Prop from '@/types/Prop';
+
+import styles from './Tab.module.scss';
 
 interface PropType<T> extends Prop<T> {
   menu: DataType[];
 }
 const TabTitle = ({ children, ...props }: Prop<HTMLLIElement>) => {
-  const { list, actions } = useTab();
+  const { list, current, actions } = useTab();
   const handleClickTab = actions.handler;
-
+  //todo: tab으로 이동후 enter키 누르면 해당 탭에 click 이벤트 발생
+  //todo: tab으로 이동후 아래 화살표 누르면 아래 카드로 focus 이동
   return (
     <>
       {list.map((item: DataType) => {
         return (
-          <li {...props} key={item.id} onClick={handleClickTab(item.id)}>
+          <li
+            role="tab"
+            tabIndex={0}
+            className={current === item.id ? styles.active : styles.title}
+            {...props}
+            key={item.id}
+            onClick={handleClickTab(item.id)}
+          >
             <Title htmlFor={item.id}>{item.content}</Title>
             {children}
           </li>
@@ -30,18 +42,28 @@ const TabTitle = ({ children, ...props }: Prop<HTMLLIElement>) => {
 const TabPanel = ({ children, ...props }: Prop<HTMLOutputElement>) => {
   const { current } = useTab();
 
-  return current === props.id && <Panel {...props}>{children}</Panel>;
+  return (
+    current === props.id && (
+      <Panel className={styles.tabPanel} {...props}>
+        {children}
+      </Panel>
+    )
+  );
 };
 
 const TabMenus = ({ children, ...props }: Prop<HTMLUListElement>) => {
-  return <ul {...props}>{children}</ul>;
+  return (
+    <ul role="tablist" className={styles.tabMenu} {...props}>
+      {children}
+    </ul>
+  );
 };
 
 const Tab = ({ menu, children, ...props }: PropType<HTMLElement>) => {
   const defaultList = menu ?? [];
 
-  const { current } = useTab();
-  const [currentId, setCurrentId] = useState<string>(current);
+  useTab();
+  const [currentId, setCurrentId] = useState<string>(defaultList[0].id);
 
   const handleClickTab = (id: string) => () => {
     setCurrentId(id);
@@ -57,7 +79,7 @@ const Tab = ({ menu, children, ...props }: PropType<HTMLElement>) => {
 
   const value = useMemo(() => contextValue, [contextValue]);
   return (
-    <section {...props}>
+    <section className={styles.tab} {...props}>
       <TabContext.Provider value={value}>{children}</TabContext.Provider>
     </section>
   );
