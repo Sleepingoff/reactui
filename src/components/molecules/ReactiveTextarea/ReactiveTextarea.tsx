5205ad38e0ad9482504746ed27c245092d3cafa5
JiHyeon Lee <justjh30@gmail.com>
2024-05-11 20:49:00 +0900

    오류 수정: enter키로 줄바꿈하는 경우 #16 (#19)
    * 기본 textarea atoms 추가
    
    * 반응형 텍스트박스 추가
    
    * horizontal 반응형 텍스트박스 추가
    
    * vertial 반응형 textarea 추가
    
    * 필요없는 코드 삭제
    
    * ♻️ ReactiveTextarea 리팩토링
    
    * 오류 수정: enter키로 줄바꿈하는 경우

--- src/components/molecules/ReactiveTextarea/ReactiveTextarea.tsx
+++ src/components/molecules/ReactiveTextarea/ReactiveTextarea.tsx
@@ -21,7 +21,7 @@ const ReactiveTextarea = ({
   rows,
   ...props
 }: PropType) => {
-  const { getGapOfBytes, splitByEnter } = useByte();
+  const { getByteOfValue, getGapOfBytes, splitByEnter } = useByte();
 
   const textarea = useRef(null);
   const [currentCols, setCurrentCols] = useState(cols ?? 1);
@@ -57,7 +57,7 @@ const ReactiveTextarea = ({
       if (isValidHorizontal) {
         setCountOfTextAfterEnter(0);
         //enter를 텍스트 중간에서 했을 경우 currentMacCols가 달라질 수 있다.
-        const currentMaxCols = calcMaxCols(value).cols;
+        const currentMaxCols = calcMaxColsByte(value);
         setCurrentCols(currentMaxCols);
       }
       if (isValidVertical) {
@@ -98,7 +98,6 @@ const ReactiveTextarea = ({
   useEffect(() => {
     if (!textarea.current) return;
     const elem = textarea.current as HTMLTextAreaElement;
-    console.log(elem);
     updateCountOfTextAfterEnter(value);
     const isOverCurrentCols = checkOverCurrentCols();
     const isPullCurrentCols = elem.cols === currentCols;
@@ -126,7 +125,7 @@ const ReactiveTextarea = ({
     setCurrentRows(elem.rows);
   }, [lines]);
 
-  const calcMaxCols = useMemo(
+  const calcMaxColsByte = useMemo(
     () => (value: string) => {
       const ArrayOfBetweenEnterValues = splitByEnter(value);
       const ArrayOfLength = ArrayOfBetweenEnterValues.map(
@@ -135,10 +134,7 @@ const ReactiveTextarea = ({
       const maxLengthOfValues = Math.max(...ArrayOfLength);
       const indexOfMaxLength = ArrayOfLength.indexOf(maxLengthOfValues);
 
-      return {
-        cols: maxLengthOfValues,
-        max: ArrayOfBetweenEnterValues[indexOfMaxLength]
-      };
+      return getByteOfValue(ArrayOfBetweenEnterValues[indexOfMaxLength]);
     },
     [value]
   );
